ddc5f1d0258b4b8604b253f70b444c57109d2c90
Osamu Moshio <osamu.moshio@gmail.com>
2025-09-15 08:34:07 +0900

    Vueコンポーネントを分割使用可能に変更

--- front-page.php
+++ front-page.php
@@ -2,7 +2,10 @@
 
 <main>
   <h1>WordPress × Vue テストページ Today</h1>
-  <div id="App"></div>
+  <!--<div id="App"></div> Vueコンポーネントをまとめて使う場合 -->
+  <div id="Hello"></div>
+	<div id="Slider"></div>
+	<div id="Tabs"></div>
 </main>
 
 <?php get_footer(); ?>
